62b674b23a194792b92d741ef0fbcd95003197b7
sirdodji <81319452+sirdodji@users.noreply.github.com>
2021-04-08 12:28:23 +0300

    Add files via upload

---
+++ lessfourtask4.py
@@ -0,0 +1,5 @@
+n = 9
+for i in range(1, n + 1):
+    for x in range(1, i + 1):
+        print(x, sep='', end='')
+    print()
---
+++ lessfourtask5.py
@@ -0,0 +1,19 @@
+string = "Ready player one"
+a = string[2]
+b = string[-2]
+c = string[:6]
+d = string[:14]
+e = string[::-1]
+f = string[::2]
+g = string[1::2]
+h = string[::-2]
+i = len(string)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(h)
+print(i)
